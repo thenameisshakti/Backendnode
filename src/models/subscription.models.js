dd5ca77581149b5dee6ba8e652f27be7066ddc2b
thename <shaktisingh@Shaktis-MacBook-Air.local>
2025-07-12 17:32:29 +0530

    this is update

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -1,5 +1,14 @@
 import mongoose,{Schema} from "mongoose";
 
-const subscriptionSchema = new Schema ({ }, Tim)
+const subscriptionSchema = new Schema ({
+    subscriber: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
+    channel : {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    }
+ }, {timestamps: true})
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
--- src/routes/user.routers.js
+++ src/routes/user.routers.js
@@ -1,9 +1,9 @@
 import { Router} from "express";
-import registerUser from "../controllers/user.controller.js";
+import registerUser, { changeCurrentPassword, getCurrentUser,getWatchHistory,getUserChannelProfile } from "../controllers/user.controller.js";
 import { upload } from "../middlewares/multer.middleware.js";
 import { verifyJWT } from "../middlewares/auth.middleware.js";
 import { loginUser } from "../controllers/user.controller.js";
-import { logoutUser } from "../controllers/user.controller.js";
+import { logoutUser,updateAccount ,updateUserAvatar,updateUserCoverImage,} from "../controllers/user.controller.js";
 import { refreshAccessToken } from "../controllers/user.controller.js";
 
 
@@ -28,6 +28,28 @@ userRouter.route("/login").post(loginUser)
 //secured routes 
 userRouter.route("/logout").post(verifyJWT,logoutUser)
 userRouter.route("/referesh-token").post(refreshAccessToken)
+userRouter.route("/change-password").post(verifyJWT,changeCurrentPassword)
+userRouter.route("/current-user").get(verifyJWT,getCurrentUser)
+
+userRouter
+.route("/update-accout")
+.patch(verifyJWT,updateAccount)
+
+userRouter
+.route("/avatar")
+.patch(verifyJWT,upload.single
+("avatar"),updateUserAvatar)
+
+userRouter
+.route('/coverImage')
+.patch(verifyJWT,upload.single
+("coverImage"),updateUserCoverImage)
+
+userRouter.route("/c/:username").get(verifyJWT,getUserChannelProfile) //becaure is req.params
+userRouter.route("/History").get(verifyJWT,getWatchHistory)
+
+
+
 
 
 export default userRouter
